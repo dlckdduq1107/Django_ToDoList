3c9d9a85912d1812f4967e239ae882b1550f4382
fansor <dlckdduq97@gmail.com>
2021-06-09 02:40:09 +0900

    MVC_create

--- my_to_do_app/models.py
+++ my_to_do_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+# 데이터 베이스와 관련된 model
+# 하나의 모델은 하나의 클래스로 표현됨
+
+class Todo(models.Model):
+    content = models.CharField(max_length=255)#데이터 형태가 CharField이고 최대길이가 255
+
--- my_to_do_app/views.py
+++ my_to_do_app/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
+
 # Create your views here.
 def index(request):
-    return render(request, "my_to_do_app/index.html")
+    todos = Todo.objects.all()
+    content = {'todos':todos}
+    return render(request, "my_to_do_app/index.html", content)
 
+def createTodo(request):
+    user_input_str = request.POST['todoContent'] # 입력칸의 name을 POST 받는다.
+    new_todo = Todo(content=user_input_str)
+    new_todo.save()
+    return HttpResponseRedirect(reverse('index'))
+    # return HttpResponse("execute create =>" +user_input_str)
