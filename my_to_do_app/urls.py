17a8744c11b5494593c7d8f97d777bc9fd973d66
fansor <dlckdduq97@gmail.com>
2021-06-25 10:21:40 +0900

    CRUD_develop

--- my_to_do_app/urls.py
+++ my_to_do_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='index'),
-    path('createTodo/', views.createTodo,name='createTodo')
+    path('createTodo/', views.createTodo,name='createTodo'),## 생성버튼을 눌렀을때
+    path('deleteTodo/', views.doneTodo,name='deleteTodo') ## 삭제버튼을 눌렀을때
 ]
--- my_to_do_app/views.py
+++ my_to_do_app/views.py
@@ -9,9 +9,17 @@ def index(request):
     content = {'todos':todos}
     return render(request, "my_to_do_app/index.html", content)
 
-def createTodo(request):
+def createTodo(request): # 데이터 생성
     user_input_str = request.POST['todoContent'] # 입력칸의 name을 POST 받는다.
     new_todo = Todo(content=user_input_str)
     new_todo.save()
     return HttpResponseRedirect(reverse('index'))
     # return HttpResponse("execute create =>" +user_input_str)
+
+def doneTodo(request):# 데이터 삭제 함수
+    done_todo_id = request.GET['todoNum'] # todoNum이라는 id를 보고 GET받아옴
+    print("완료한 todo의 id ", done_todo_id)
+    todo = Todo.objects.get(id=done_todo_id) # 데이터베이스에서 해당 id에 해당하는 값 가져옴
+    todo.delete() # 데이터 삭제
+    return HttpResponseRedirect(reverse('index'))
+    
